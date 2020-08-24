96e1b6787351726e832e9ebfc28ff6426787ea4a
Roman Khomenko <romankhomenko1995@gmail.com>
2020-08-24 13:26:49 +0100

    Added database init

--- data-service/app.py
+++ data-service/app.py
@@ -1,11 +1,10 @@
 import os
 
 from flask import Flask, Response
-import sqlalchemy as db
+from flask_sqlalchemy import SQLAlchemy
 
 from utils.RandomDealData import RandomDealData
 
-app = Flask(__name__)
 
 db_host = os.environ['DB_HOST']
 db_name = os.environ['DB_NAME']
@@ -13,9 +12,19 @@ db_user = os.environ['DB_USER']
 db_password = os.environ['DB_PASSWORD']
 conn_url = f'mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}'
 
-print('Trying to create engine with link', conn_url)
-engine = db.create_engine(conn_url)
-print('Engine created')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = conn_url
+db = SQLAlchemy(app)
+
+
+class Deal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    counter_party = db.Column(db.String(100))
+
+
+db.create_all()
+db.session.commit()
 
 
 @app.route('/')
