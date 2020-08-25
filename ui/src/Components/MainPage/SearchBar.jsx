099022079c2458c38e6040e7670fe93e8bc9e571
Natalia Ponomareva <nataliya.ponomareva.mm@gmail.com>
2020-08-25 08:31:56 +0100

    Updated report Modal

--- ui/src/Components/MainPage/SearchBar.jsx
+++ ui/src/Components/MainPage/SearchBar.jsx
@@ -1,52 +1,67 @@
 import React from "react";
+import ReportModal from '../ReportModal/ReportModal'
 
 
   class SearchBar extends React.Component {
     constructor(props) {
       super(props);
 
-      this.state = {filterType: 'instrument'};
-      this.state = {NameType: 'Name'};
+      this.state = {filterType: 0};
+      this.state = {dealer: 0};
     }
 
     handleFilterTypeChanged = (event) => {
       this.setState({filterType: event.target.value});
     }
 
-    handleFilterTextChanged = (event) => {
-      this.setState({filterText: event.target.value});
+    handleDealerChanged = (event) => {
+          this.setState({dealer: event.target.value});
     }
-
-    handleSearchClick = () => {
-      alert(this.state.filterType);
-    }
-
-    handleNameTypeChanged = (event) => {
-          this.setState({NameType: event.target.value});
-    }
-
-    handleNameTextChanged = (event) => {
-          this.setState({NameText: event.target.value});
-      }
 
 
     render() {
+      // axios request for them
+      let dealers = [
+        {name: "Anna", surname: "Agapova"},
+        {name: "Ivan", surname: "Ivanov"}
+      ];
+
+      let rows = dealers.map((dealer, i) => (
+              <option value={i}>{dealer.name} {dealer.surname}</option>
+      ));
+
+      // axios request for them
+      let instruments = [
+        {name: "Instrument name"},
+        {name: "Conterparty"},
+        {name: "Conterparty"}
+      ];
+
+      let rows2 = instruments.map((instrument, i) => (
+              <option value={i}>{instrument.name}</option>
+      ));
+
       return (
+
         <>
           <div class="search_bar">
             <p class="search_bar_instrustion">If you need to see specific information, please use filters below</p>
             <div class="select_row">
-                <select class="select_row_item" value={this.state.filterType} onChange={this.handleFilterTypeChanged}>
-                  <option value='instrument'>Instrument name</option>
-                  <option value='conterparty'>Conterparty</option>
-                  <option value='price'>Price</option>
+                <select class="select_row_item" 
+                  value={this.state.filterType} 
+                  onChange={this.handleFilterTypeChanged}
+                >
+                  {rows2}
                 </select>
-                <button class="select_row_item"> Get aggregated report</button>
-                <select class="select_row_item" value={this.state.NameType} onChange={this.handleNameTypeChanged}>
-                    <option value='Name'>Name</option>
-                    <option value='John'>John</option>
-                    <option value='Lina'>Lina</option>
-                    <option value='Andrew'>Andrew</option>
+                <ReportModal
+                  dealer={this.state.dealer}
+                  instrument={this.state.filterType}
+                />
+                <select class="select_row_item" 
+                  value={this.state.dealer} 
+                  onChange={this.handleDealerChanged}
+                  >
+                    {rows}
                 </select>
             </div>
           </div>
